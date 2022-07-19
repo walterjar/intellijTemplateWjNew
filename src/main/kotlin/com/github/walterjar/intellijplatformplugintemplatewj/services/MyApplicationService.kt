30ad4ac0ac1cf6762a365038f34ae70c0ecb500f
GitHub Action <action@github.com>
2022-07-19 23:45:11 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/walterjar/intellijplatformplugintemplatewj/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.walterjar.intellijplatformplugintemplatewj.services
+
+import com.github.walterjar.intellijplatformplugintemplatewj.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
